3f3dc115bb1ab9a62d0c40c88fa3a7d709eb66ac
KavinilaLoganathan-2507 <kavinilaloganathan@gmail.com>
2026-01-23 16:32:45 +0100

    Fix comments and format code in Day_2.c

--- C/Day_2.c
+++ C/Day_2.c
@@ -1,1 +1,14 @@
-//Declaring a variable and printing 
+//Declaring a variable and printing
+#include <stdio.h>
+int main()
+{
+  int num = 10;
+  char name[] = "Hello world";
+  float fam = .6;
+
+  printf("The number is %d \n ", num);
+  printf("The name is %s \n ", name);
+  printf("The float is %f \n", fam);
+
+  return 0 ;
+}

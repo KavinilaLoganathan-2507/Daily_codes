3908081dbdcf55a166af6a771351fb1f5bb22e86
KavinilaLoganathan-2507 <kavinilaloganathan@gmail.com>
2026-01-23 16:23:07 +0100

    Add variable declarations and print statements

--- Python/Day_2 .py
+++ Python/Day_2 .py
@@ -1,1 +1,7 @@
 #Declaring a variable and printing 
+num_1 = 20
+string_1 = "Hello world"
+num_2 = 0.6
+print("The number is ", num_1)
+print("The string  is ", string_1)
+print("The float is ", num_2)

28437b01993a43165e8cf8d24a630c231ea8a63c
KavinilaLoganathan-2507 <kavinilaloganathan@gmail.com>
2026-01-24 17:11:29 +0100

    Add Day_3.c for number and name input

---
+++ C/Day_3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    char name[10];
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("The number is: %d\n", num);
+
+    printf("Enter a name: ");
+    scanf("%s", name);
+    printf("The name is: %s\n", name);
+
+    return 0;
+}

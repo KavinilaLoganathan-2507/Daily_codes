287c51cd84b34b5a03e2cd85912939f33f0bc4e4
KavinilaLoganathan-2507 <kavinilaloganathan@gmail.com>
2026-01-25 17:29:49 +0100

    Add arithmetic operations in Day_4.py

---
+++ Python/Day_4.py
@@ -0,0 +1,21 @@
+#Arithmetic operation
+num_1 = int(input("Enter first number ")
+num_2 = int(input("Enter second number ")
+print("The addition is ",num_1 + num_2)
+print("The difference is ",num_1 - num_2)
+print("The product is ",num_1 * num_2)
+print("The reminder on division is ",num_1 % num_2)
+print("The division is ",num_1 / num_2)
+print("The divion without float is ",num_1 // num_2)
+print("The expon is ",num_1 ** num_2)
+
+
+
+
+
+
+
+
+
+
+            

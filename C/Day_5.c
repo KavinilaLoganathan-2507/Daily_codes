6367233f22e7287ae3f89b33030ecbde969d68f7
KavinilaLoganathan-2507 <kavinilaloganathan@gmail.com>
2026-01-26 19:02:25 +0100

    Implement relational operator comparisons in Day_5.c
    Added relational operator comparisons for two numbers.

---
+++ C/Day_5.c
@@ -0,0 +1,15 @@
+//Relational operator or comparative operator 
+#include <stdio.h>
+int main()
+{
+int num_1, num_2;
+
+printf("num_1 > num_2: %d \n", num_1 > num_2);
+printf("num_1 >= num_2: %d \n", num_1 >= num_2);
+printf("num_1 <= num_2: %d \n", num_1 <= num_2);
+printf("num_1 < num_2: %d \n", num_1 < num_2);
+printf("num_1 == num_2: %d \n", num_1 == num_2);
+printf("num_1 != num_2: %d \n", num_1 != num_2);
+
+return 0;
+}

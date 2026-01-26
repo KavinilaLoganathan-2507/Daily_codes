6c4ef349ae83316c0df0c04b5ecf4bc4e7422fc5
KavinilaLoganathan-2507 <kavinilaloganathan@gmail.com>
2026-01-26 19:08:25 +0100

    Implement relational operators for user inputs
    Add comparisons for two user-input numbers.

---
+++ Python/Day_5.py
@@ -0,0 +1,10 @@
+#Relational or comparative operator
+num_1 = int(input("Enter a number:"))
+num_2 = int(input("Enter next number:"))
+print(num_1==num_2)
+print(num_1!=num_2)
+print(num_1<=num_2)
+print(num_1>=num_2)
+print(num_1>num_2)
+print(num_1<num_2)
+

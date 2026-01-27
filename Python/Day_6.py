8b4b7f117fd11c30eec72c4f0780bcb0550e51b9
KavinilaLoganathan-2507 <kavinilaloganathan@gmail.com>
2026-01-27 18:07:47 +0100

    Create Day_6.py

---
+++ Python/Day_6.py
@@ -0,0 +1,6 @@
+#Relational operators 
+num_1 = int(input("Enter first number:"))
+num_2 = int(input("Enter second number:"))
+print("Using AND operator", num_1 & num_2)
+print("Using OR operator", num_1 | num_2)
+print("Using NOT operator", ~num_1 )

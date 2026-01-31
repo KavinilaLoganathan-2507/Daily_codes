25a6221bd4a3f10afc897dab2b9522250f4cd38e
KavinilaLoganathan-2507 <kavinilaloganathan@gmail.com>
2026-01-31 16:58:11 +0100

    Create Day_7.c

---
+++ C/Day_7.c
@@ -0,0 +1,12 @@
+//Get a number from the user, subtract 5 from that number, and print the result.
+#include <stdio.h>
+int main()
+{
+    int num;
+printf("Enter a number:");
+  scanf("%d" , &num);
+  printf("Result is : %d \n",num+5);
+
+
+  return 0;
+}

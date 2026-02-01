f78de7bab56e085dfa054be3aa28daf7a9f651fc
KavinilaLoganathan-2507 <kavinilaloganathan@gmail.com>
2026-02-01 18:46:29 +0100

    Create Day_8.c

---
+++ C/Day_8.c
@@ -0,0 +1,12 @@
+// Get a number from user and multiply 3 to that number and print the result.
+#include<stdio.h>
+int main()
+{
+    int num;
+printf("Enter a number:");
+  scanf("%d", &num);
+    printf("multiplied number is %d\n", num*3);
+
+  
+  return 0 ;
+}

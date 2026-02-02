8cc05f7fd6771106d3b7d36fc077abf90df75eef
KavinilaLoganathan-2507 <kavinilaloganathan@gmail.com>
2026-02-02 16:46:06 +0100

    Create Day_9.c

---
+++ C/Day_9.c
@@ -0,0 +1,12 @@
+//Dividing by 6
+#include <stdio.h>
+
+int main() {
+    int num, result;
+    
+    printf("Enter a number:");
+    scanf("%d", &num);
+result = num/6;
+printf("result %d\n",result);
+    return 0;
+}

63bb55f772bbb449fe1d9660cde3c1071e7f66d3
KavinilaLoganathan-2507 <kavinilaloganathan@gmail.com>
2026-02-04 21:34:58 +0530

    Create Day_11.c

---
+++ C/Day_11.c
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter a two digit number:");
+    scanf("%d",&num);
+    printf("The result is : %d \n",num/10);
+
+    return 0;
+}

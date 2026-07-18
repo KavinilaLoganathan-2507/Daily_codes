29fb8ab1d93399fa54d69c81e52672dca011e1d4
KavinilaLoganathan-2507 <kavinilaloganathan@gmail.com>
2026-07-18 20:56:02 +0530

    Add logic to subtract 5 from odd input number
    Implement logic to subtract 5 from an odd number without using 'if'.

---
+++ Python/DAy_23.py
@@ -0,0 +1,6 @@
+# Get a number from the user and subtract 5 from that number if the number is odd, then print the result. Do not use “if”. 
+num = int(input("Enter a number:"))
+if num%2 != 0:
+    print("Outpt:", num + 5 )
+else:
+    print("No addition",num)

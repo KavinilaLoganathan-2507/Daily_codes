40d74fa49a9abbd26ea50c9a18b6094dde310043
KavinilaLoganathan-2507 <kavinilaloganathan@gmail.com>
2026-07-23 07:18:01 +0530

    Add binary search function to Easy.py

--- Easy.py
+++ Easy.py
@@ -10,3 +10,28 @@ for i in range (len(numbers)):
     for j in range(i+1, len(numbers)):
         if numbers[i] == numbers[j]:
             print("duplicate",numbers[i])
+
+# Binary search 
+
+def binary_search_iterative(arr, target):
+    left = 0
+    right = len(arr) - 1
+    
+    while left <= right:
+     
+        mid = left + (right - left) // 2
+        
+      
+        if arr[mid] == target:
+            return mid
+      
+        elif arr[mid] < target:
+            left = mid + 1
+    
+        else:
+            right = mid - 1
+        
+    return 0
+
+my_list = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91]
+print(binary_search_iterative(my_list , 5 ))

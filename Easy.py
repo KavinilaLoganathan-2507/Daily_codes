4d5da183afca98502b0c605d2df90a0ff1736f44
KavinilaLoganathan-2507 <kavinilaloganathan@gmail.com>
2026-07-23 06:56:48 +0530

    Implement duplicate finding in Easy.py
    Added a nested loop to find and print duplicate numbers in a list.

--- Easy.py
+++ Easy.py
@@ -3,3 +3,10 @@
 n = int(input("Enter a number:"))
 for i in range(1,n):
   print(i)
+# Finding Duplicate using for loop
+
+numbers = [1,2,3,4,3,5,2]
+for i in range (len(numbers)):
+    for j in range(i+1, len(numbers)):
+        if numbers[i] == numbers[j]:
+            print("duplicate",numbers[i])

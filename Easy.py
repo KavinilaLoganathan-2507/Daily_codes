9820e58a40a38b7bed18852f1c0117448b2cbb1d
KavinilaLoganathan-2507 <kavinilaloganathan@gmail.com>
2026-07-26 17:54:53 +0530

    Add various Python programs for user input tasks

--- Easy.py
+++ Easy.py
@@ -14,6 +14,8 @@ for i in range (len(numbers)):
 # Output:
 # duplicate 2
 # duplicate 3
+#___________________________________________________________________________________________________________________________________________________________________
+
 
 # Binary search 
 
@@ -43,6 +45,8 @@ print(binary_search_iterative(my_list , 5 ))
 # Output:
 # 1 #index value of 5 
 
+#_____________________________________________________________________________________________________________________________________________________________________
+
 # Get a number from user and square them 
 
 num = int(input("Enter a number: "))
@@ -52,3 +56,105 @@ print("Square =", square)
 # Output:
 # Enter a number: 5
 # Square = 25
+
+#______________________________________________________________________________________________________________________________________________________________________
+
+
+#Write a Python program to:
+#Read two integers from the user.
+#Swap their values.
+#Print the values after swapping.
+#without using if or loops 
+num_1 = int(input("Enter a two digit number :"))
+num_2 = int(input("Enter a two digit number:"))
+print("num_1", num_2)
+print("num_2", num_1)
+
+#output:
+#Enter a two digit number:25
+#Enter a two digit number:45
+#num_1 = 45
+#num_2 = 25
+
+#________________________________________________________________________________________________________________________________________________________________________
+
+#Write a Python program to:
+#Read an integer from the user.
+#Check whether the number is even or odd.
+#Print the result.
+
+num = int(input("Enter a number :"))
+if num <= 0 :
+      print("Enter a valid Positive integer")
+elif num%2 == 0 :
+       print("The given number is even")
+else:
+       print("The given number is odd")
+
+#Output :
+#Enter a number: 8
+# The given number is even 
+
+
+#______________________________________________________________________________________________________________________________________________________________________________
+
+
+#Write a Python program to:
+#Read three integers from the user.
+#Find the largest number.
+#Print the largest number.
+
+
+num_1 = int(input("Enter a number:"))
+num_2 = int(input("Enter a number:"))
+num_3 = int(input("Enter a number:"))
+
+if num_1 > num_2 and num_1 > num_3:
+  print("num_1 is the largest number ")
+elif num_2 > num_1 and num_2 > num_3:
+  print("num_2 is the largest number ")
+else:
+  print("num_3 is the largest number")
+
+#output:
+#Enter a number:1
+#Enter a number:2
+#Enter a number:3
+#num_3 is the largest number 
+
+#______________________________________________________________________________________________________________________________________________________________________________________
+
+
+#Write a Python program to:
+#Read a 3-digit integer from the user.
+#Find the sum of its digits.
+#Print the result.
+
+num = int(input("Enter a three digit-number:"))
+hundreds_digit = num // 100 
+ones_digit = (num % 10) 
+tens_digit = (num // 10 ) % 10 
+print(res + res_2 + res_3)
+
+#Output:
+# Enter a three-digit number:123
+# 6 
+
+#___________________________________________________________________________________________________________________________________________________________________________________________
+
+#Write a Python program to:
+#read a 3-digit integer from the user.
+#Reverse the digits.
+#Print the reversed number.
+
+
+num = int(input("Enter a three digit-number:"))
+hundreds_digit = num // 100 
+ones_digit = (num % 10) 
+tens_digit = (num // 10 ) % 10
+# print(ones_digit,tens_digit,hundreds_digit) # with space inbetween 
+reverse = ones_digit * 100 + tens_digit * 10 + hundreds_digit
+print(reverse)
+#Output:
+#Enter a three-digit number: 123
+#321

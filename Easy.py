7949662c06db7430f2593954708ed5a544c9e570
KavinilaLoganathan-2507 <kavinilaloganathan@gmail.com>
2026-08-10 14:24:01 +0530

    Optimize prime number check loop
    Updated the prime number check loop to optimize performance.

--- Easy.py
+++ Easy.py
@@ -525,7 +525,7 @@ num = int(input("Enter a number :"))
 if num <= 1:
     print(num, "is not a prime number")
 else:
-    for i in range(2, num):
+    for i in range(2, num): #(2,(num**0.5) +1)
         if (num % i) == 0:
             print(num, "is not a prime number")
             break
